dc5c5d2afaa69d000416f8a1838c217888abb875
Toacy Oliveira <toacy@users.noreply.github.com>
2022-01-10 10:42:57 -0500

    Updating Payment Delegate to indicate travel items are being used in Travel Complete

--- travelTutorialCOMPLETE/src/main/java/org/agilekip/tutorials/travelcomplete/delegates/PaymentDelegate.java
+++ travelTutorialCOMPLETE/src/main/java/org/agilekip/tutorials/travelcomplete/delegates/PaymentDelegate.java
@@ -1,9 +1,19 @@
 package org.agilekip.tutorials.travelcomplete.delegates;
 
 import java.math.BigDecimal;
+import org.agilekip.tutorials.travelcomplete.domain.Car;
+import org.agilekip.tutorials.travelcomplete.domain.Flight;
+import org.agilekip.tutorials.travelcomplete.domain.HotelRoom;
 import org.agilekip.tutorials.travelcomplete.domain.TravelPlan;
 import org.agilekip.tutorials.travelcomplete.domain.enumeration.PlanStatus;
+import org.agilekip.tutorials.travelcomplete.repository.CarRepository;
+import org.agilekip.tutorials.travelcomplete.repository.FlightRepository;
+import org.agilekip.tutorials.travelcomplete.repository.HotelRoomRepository;
 import org.agilekip.tutorials.travelcomplete.repository.TravelPlanRepository;
+import org.agilekip.tutorials.travelcomplete.service.dto.CarDTO;
+import org.agilekip.tutorials.travelcomplete.service.dto.FlightDTO;
+import org.agilekip.tutorials.travelcomplete.service.dto.HotelRoomDTO;
+import org.agilekip.tutorials.travelcomplete.service.dto.HotelRoomDTO;
 import org.agilekip.tutorials.travelcomplete.service.dto.TravelPlanDTO;
 import org.agilekip.tutorials.travelcomplete.service.dto.TravelPlanProcessDTO;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -15,9 +25,20 @@ import org.springframework.stereotype.Component;
 public class PaymentDelegate implements JavaDelegate {
 
     private final TravelPlanRepository travelPlanRepo;
+    private final HotelRoomRepository hotelRoomRepo;
+    private final FlightRepository flightRepo;
+    private final CarRepository carRepo;
 
-    public PaymentDelegate(TravelPlanRepository travelPlanRepository) {
+    public PaymentDelegate(
+        TravelPlanRepository travelPlanRepository,
+        HotelRoomRepository hotelRoomRepo,
+        FlightRepository flightRepo,
+        CarRepository carRepo
+    ) {
         this.travelPlanRepo = travelPlanRepository;
+        this.hotelRoomRepo = hotelRoomRepo;
+        this.carRepo = carRepo;
+        this.flightRepo = flightRepo;
     }
 
     @Override
@@ -31,5 +52,19 @@ public class PaymentDelegate implements JavaDelegate {
 
         // save the modified obj to the repo
         travelPlanRepo.save(travelPlanObj);
+
+        HotelRoom hotelRoom = this.hotelRoomRepo.findById(travelPlanDTO.getHotelRoom().getId()).get();
+        hotelRoom.setBoodked(travelPlanDTO.getHotelStartDate());
+        hotelRoom.setDuration(travelPlanDTO.getHotelDuration());
+        hotelRoomRepo.save(hotelRoom);
+
+        Flight flight = this.flightRepo.findById(travelPlanDTO.getFlight().getId()).get();
+        flight.setEmptySeats(flight.getEmptySeats() - travelPlanDTO.getNumPax());
+        flightRepo.save(flight);
+
+        Car car = this.carRepo.findById(travelPlanDTO.getCar().getId()).get();
+        car.setBooked(travelPlanDTO.getCarStartDate());
+        car.setDuration(travelPlanDTO.getCarDuration());
+        carRepo.save(car);
     }
 }
