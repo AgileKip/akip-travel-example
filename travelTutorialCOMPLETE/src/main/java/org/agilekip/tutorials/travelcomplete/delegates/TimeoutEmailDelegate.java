34d301126a1bb8cd883a018b649246dcd35524e0
Toacy Oliveira <toacy@users.noreply.github.com>
2022-01-11 15:28:42 -0500

    Configuring Emails for TRavel Complete

---
+++ travelTutorialCOMPLETE/src/main/java/org/agilekip/tutorials/travelcomplete/delegates/TimeoutEmailDelegate.java
@@ -0,0 +1,36 @@
+package org.agilekip.tutorials.travelcomplete.delegates;
+
+import java.util.Locale;
+import org.agilekip.tutorials.travelcomplete.service.MailService;
+import org.agilekip.tutorials.travelcomplete.service.dto.TravelPlanDTO;
+import org.agilekip.tutorials.travelcomplete.service.dto.TravelPlanProcessDTO;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+@Component
+public class TimeoutEmailDelegate implements JavaDelegate {
+
+    @Autowired
+    MailService mailService;
+
+    @Autowired
+    SpringTemplateEngine templateEngine;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        TravelPlanProcessDTO pi = (TravelPlanProcessDTO) delegateExecution.getVariable("processInstance");
+
+        TravelPlanDTO travelPlan = pi.getTravelPlan();
+
+        String to = "xxx@gmail.com";
+        String subject = "[Travel Complete] Confirmation email " + travelPlan.getName();
+        Context context = new Context(Locale.getDefault());
+        context.setVariable("travelPlan", travelPlan);
+        String content = templateEngine.process("travelPlanProcess/timeoutEmail", context);
+        mailService.sendEmail(to, subject, content, false, true);
+    }
+}
