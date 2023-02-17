222030643b454603eef9bccc12d97bc8db5cc9c8
CassioJr <ows.cassio@loginlogistica.com.br>
2023-02-17 09:25:08 -0300

    DMN example implementation

--- travelTutorialDMN/src/main/java/com/mycompany/myapp/config/DMNConfiguration.java
+++ travelTutorialDMN/src/main/java/com/mycompany/myapp/config/DMNConfiguration.java
@@ -1,11 +1,7 @@
 package com.mycompany.myapp.config;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.repository.Deployment;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +12,13 @@ public class DMNConfiguration {
     @Autowired
     RepositoryService repositoryService;
 
+    /**
+     * Esse método é utilizado para a implantação do DMN
+     * @filePath Recebe o caminho do arquivo DMN
+     */
     @Bean
     public Deployment deployDMN() {
-        String f = "./bpmn/CalculateDays.dmn";
-        return repositoryService.createDeployment().addClasspathResource(f).deploy();
+        String filePath = "./bpmn/CalculateDays.dmn";
+        return repositoryService.createDeployment().addClasspathResource(filePath).deploy();
     }
 }
